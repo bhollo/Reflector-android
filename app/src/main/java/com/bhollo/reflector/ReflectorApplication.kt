3e21fd4c1b1a1655c0200116a84732a993a561af
DerrickRocha <drocha616@gmail.com>
2019-08-15 17:34:22 -0600

    Added admob ads. Moves strings to xml.

--- app/src/main/java/com/bhollo/reflector/ReflectorApplication.kt
+++ app/src/main/java/com/bhollo/reflector/ReflectorApplication.kt
@@ -1,12 +1,14 @@
 package com.bhollo.reflector
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import com.testfairy.TestFairy
 
 class ReflectorApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        TestFairy.begin(this, "SDK-eTDNMh8y");
+        TestFairy.begin(this, "SDK-eTDNMh8y")
+        MobileAds.initialize(this)
     }
 }
