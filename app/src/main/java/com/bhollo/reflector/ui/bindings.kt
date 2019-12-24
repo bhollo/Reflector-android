a784ff2c2ced9b1b4aad16219e41cebb6633be93
Derrick Rocha <drocha616@gmail.com>
2019-12-23 21:37:00 -0700

    Production ads not loading (#9)
    * Updated admob sdk.
    
    * Removed unused code.

--- app/src/main/java/com/bhollo/reflector/ui/bindings.kt
+++ app/src/main/java/com/bhollo/reflector/ui/bindings.kt
@@ -12,7 +12,7 @@ fun setupAd(view: AdView, flavor: String){
     if (flavor.contentEquals("free")){
         val adRequest = AdRequest
             .Builder()
-           // .addTestDevice("5E32C5BA282ED1BC45704B9F4E8D3FFD")
+            //.addTestDevice("5E32C5BA282ED1BC45704B9F4E8D3FFD")
             .build()
         view.loadAd(adRequest)
         view.show()
