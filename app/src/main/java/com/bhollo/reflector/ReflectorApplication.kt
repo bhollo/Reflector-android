2d65f93e0cee41bb46011ad70675684b408eca27
Derrick Rocha <drocha616@gmail.com>
2019-12-23 21:37:00 -0700

    VersionCode and versionName bump.

--- app/src/main/java/com/bhollo/reflector/ReflectorApplication.kt
+++ app/src/main/java/com/bhollo/reflector/ReflectorApplication.kt
@@ -3,13 +3,11 @@ package com.bhollo.reflector
 import android.app.Application
 import com.bhollo.reflector.extensions.isFreeVersion
 import com.google.android.gms.ads.MobileAds
-import com.testfairy.TestFairy
 
 class ReflectorApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        TestFairy.begin(this, "SDK-eTDNMh8y")
         if (isFreeVersion()) {
             MobileAds.initialize(this)
         }
